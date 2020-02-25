e2cd158c4bf0af0363a18a69690164c967ff337f
Batyrkhan <musichannelg@gmail.com>
2020-02-25 12:54:06 +0600

    merge sort added

--- app.js
+++ app.js
@@ -129,6 +129,45 @@ productNames.forEach(function(item) {
   });
 });
 
+// Merge Sort Implentation (Recursion)
+function merge (left, right) {
+  let resultArray = [], leftIndex = 0, rightIndex = 0;
+
+  // We will concatenate values into the resultArray in order
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex][4] > right[rightIndex][4]) {
+      resultArray.push(left[leftIndex]);
+      leftIndex++; // move left array cursor
+    } else {
+      resultArray.push(right[rightIndex]);
+      rightIndex++; // move right array cursor
+    }
+  }
+  // We need to concat here because there will be one element remaining
+  // from either left OR the right
+  return resultArray
+          .concat(left.slice(leftIndex))
+          .concat(right.slice(rightIndex));
+}
+
+function mergeSort (unsortedArray) {
+  // No need to sort the array if the array only has one element or empty
+  if (unsortedArray.length <= 1) {
+    return unsortedArray;
+  }
+  // In order to divide the array in half, we need to figure out the middle
+  const middle = Math.floor(unsortedArray.length / 2);
+
+  // This is where we will be dividing the array into left and right
+  const left = unsortedArray.slice(0, middle);
+  const right = unsortedArray.slice(middle);
+
+  // Using recursion to combine the left and right
+  return merge(
+    mergeSort(left), mergeSort(right)
+  );
+}
+
 app.get("/", function(req, res) {
   res.render("home");
 });
@@ -271,21 +310,7 @@ app.post("/sort", function(req, res) {
   }
   if (sortType === "Rating") {
     const productNamesSortedRating = Array.from(productNames);
-    var swapp;
-    var n = productNamesSortedRating.length - 1;
-    var x = productNamesSortedRating;
-    do {
-      swapp = false;
-      for (var i = 0; i < n; i++) {
-        if (x[i][4] < x[i + 1][4]) {
-          var temp = x[i];
-          x[i] = x[i + 1];
-          x[i + 1] = temp;
-          swapp = true;
-        }
-      }
-      n--;
-    } while (swapp);
+    var x = mergeSort(productNamesSortedRating);
     res.render("shop_sort_by_rating", {
       productNamesSortedRating: x
     });
